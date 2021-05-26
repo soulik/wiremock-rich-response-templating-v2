86539cc909f3f353b0480ab8cb60b4f138ebb54f
Kazuki Negoro <negokaz@gmail.com>
2021-05-26 23:40:49 +0900

    ✨feat: Implement ComparisionHelpers

---
+++ src/main/java/io/github/negokaz/wiremock/responsetemplating/rich/ComparisonHelpers.java
@@ -0,0 +1,45 @@
+package io.github.negokaz.wiremock.responsetemplating.rich;
+
+import wiremock.com.github.jknack.handlebars.Helper;
+import wiremock.com.github.jknack.handlebars.Options;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+public enum ComparisonHelpers implements Helper<Object> {
+
+    compare {
+        @Override
+        protected Object safeApply(Object context, Options options) {
+            final BigDecimal lValue = new BigDecimal(context.toString());
+            final String op = options.param(0).toString();
+            final BigDecimal rValue = new BigDecimal(options.param(1).toString());
+            switch (op) {
+                case ">":
+                    return lValue.compareTo(rValue) > 0;
+                case ">=":
+                    return lValue.compareTo(rValue) >= 0;
+                case "<":
+                    return lValue.compareTo(rValue) < 0;
+                case "<=":
+                    return lValue.compareTo(rValue) <= 0;
+                case "==":
+                    return lValue.compareTo(rValue) == 0;
+                default:
+                    return false;
+            }
+        }
+    };
+
+    @Override
+    public Object apply(Object context, Options options) throws IOException {
+        if (options.isFalsy(context)) {
+            Object param = options.param(0, null);
+            return param == null ? null : param.toString();
+        } else {
+            return safeApply(context, options);
+        }
+    }
+
+    protected abstract Object safeApply(Object context, Options options);
+}
