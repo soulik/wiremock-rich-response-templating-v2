73aa53c84ef637a362968c0eee0af78eadcd7e6f
Kazuki Negoro <negokaz@gmail.com>
2021-08-02 08:49:13 +0900

    ✨feat: Add an entrypoint GlobalRichResponseTemplateTransformer

---
+++ src/main/java/com/github/negokaz/wiremock/transformer/GlobalRichResponseTemplateTransformer.java
@@ -0,0 +1,8 @@
+package com.github.negokaz.wiremock.transformer;
+
+public class GlobalRichResponseTemplateTransformer extends RichResponseTemplateTransformer {
+
+    public GlobalRichResponseTemplateTransformer() {
+        super(true);
+    }
+}
